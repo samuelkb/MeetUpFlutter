509bf8e17b50a9a779c1e972250dd26576a753e3
Hernandez Carmona Samuel Karim <MB78200@cb.bbvabancomer.com.mx>
2019-08-14 11:55:50 -0500

    Construccion de vista value card

--- lib/src/views/valuecard.dart
+++ lib/src/views/valuecard.dart
@@ -1,16 +1,43 @@
 import 'package:flutter/material.dart';
 
 class ValueCard extends StatefulWidget {
-  ValueCard({Key key}) : super(key: key);
-
+  @override
   _ValueCardState createState() => _ValueCardState();
 }
 
 class _ValueCardState extends State<ValueCard> {
+  bool flag = false;
   @override
   Widget build(BuildContext context) {
-    return Container(
-       child: Text("Vista de ValueCard"),
+
+    final String item = ModalRoute.of(context).settings.arguments;
+    
+    return Scaffold(
+        body: GestureDetector(
+          onTap: (){
+            print(flag);
+            if(flag==false){
+            flag=true;
+            setState(()=>flag);
+            }else{
+            flag=false;
+            setState(()=>flag);
+            }
+          },
+            child: Container(
+            color: Colors.blue,  
+            child: Center(
+            child: _value(flag,item)),
+      ),
+        ),
     );
+  }
+
+  Widget _value(bool flag,String item){
+    if(flag){
+      return Text(item,style: TextStyle(color: Colors.white,fontSize: 170.0,fontWeight: FontWeight.bold),);
+    }else{
+      return Container(color: Colors.blueAccent,);
+    }
   }
 }
